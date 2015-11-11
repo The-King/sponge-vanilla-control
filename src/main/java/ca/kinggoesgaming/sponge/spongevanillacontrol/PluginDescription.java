75ed43fbe99be0388c31fe2158d4a6ccbe1048bb
The-King <hunar911@gmail.com>
2015-11-10 18:28:22 -0800

    *changed the plugin id

--- src/main/java/ca/kinggoesgaming/sponge/spongevanillacontrol/PluginDescription.java
+++ src/main/java/ca/kinggoesgaming/sponge/spongevanillacontrol/PluginDescription.java
@@ -30,7 +30,7 @@ package ca.kinggoesgaming.sponge.spongevanillacontrol;
  */
 public class PluginDescription {
 
-    public static final String ID = "sponge-vanilla-control";
+    public static final String ID = "SpongeVanilaControl";
     public static final String NAME = "SpongeVanilaControl";
     public static final String VERSION = "1.0-SNAPSHOT";
 }
